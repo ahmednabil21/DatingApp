f3dfb1bab2cd489e9a606df9acb971bfff82b3ca
ahmednabil21 <150255423+ahmednabil21@users.noreply.github.com>
2023-12-19 09:10:05 -0800

    Update Program.cs
    add builder.Services.AddSwaggerGen

--- API/Program.cs
+++ API/Program.cs
@@ -5,24 +5,48 @@ using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 
-
-//Add DataCntext and Config With fremwork >>
+// Add DbContext
 builder.Services.AddDbContext<DataContext>(opt => 
-{opt.UseSqlite(builder.Configuration.GetConnectionString("DefultConnection"));} );
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-
-// Add services to the container.
+// Add Swagger services
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API Name", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name V1");
+    });
+}
+// Add services to the container.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name V1");
+    });
+}
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
-//app.Run();
+app.Run();
 
